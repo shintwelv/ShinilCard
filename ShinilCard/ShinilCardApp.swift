81b64a2bccac6500d0b999ed31e866fad44348b2
elvinHeo <shinilheo0205@gmail.com>
2022-10-04 22:25:58 +0900

    Initial Commit

---
+++ ShinilCard/ShinilCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShinilCardApp.swift
+//  ShinilCard
+//
+//  Created by ShinIl Heo on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct ShinilCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
